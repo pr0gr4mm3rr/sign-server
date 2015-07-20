2401818c48354c59f333de6115ccf05b2fcc0a4a
pr0gr4mm3rr <pr0gr4mm3rr@gmail.com>
2015-07-20 16:49:53 -0400

    Added ability to remove tasks from jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var express = require('express');
 var router = module.exports = express.Router();
 
@@ -32,6 +33,8 @@ router.post('/create', function(req, res, next) {
       error: 'Job must have a name'
     });
 
+
+
   // Make sure it doesn't exist
   db.models.Job.exists({
     name: req.body.name
@@ -45,10 +48,10 @@ router.post('/create', function(req, res, next) {
       name: req.body.name
     }, function(err, results) {
       if (err)
-        // XXX Do we want to send a straight error to the client?
+      // XXX Do we want to send a straight error to the client?
         return res.json({
-          error: err
-        });
+        error: err
+      });
 
       res.json({
         success: true
@@ -60,34 +63,80 @@ router.post('/create', function(req, res, next) {
 
 /* POST to add a task to a job */
 router.post('/:job/addtask', function(req, res, next) {
-  // Get job
-  db.models.Job.get(req.params.job, function(err, job) {
-    if (err)
-      return res.json({
-        error: err
-      });
+  async.parallel([
+    function(cb) {
+      // Get job
+      db.models.Job.get(req.params.job, cb);
+    },
+    function(cb) {
+      // Get task
+      db.models.Task.get(req.body.task, cb);
+    }
+  ], function(err, results) {
+    if (err) return next(err);
 
-      console.log(Object.keys(job));
-    // Get task as well
-    db.models.Task.get(req.body.task, function(err, task) {
-      if (err)
-        return res.json({
-          error: err
-        });
+    var job = results[0];
+    var task = results[1];
 
-      // Add task to job
-      job.addTasks(task, function(err) {
-        if (err)
-          return res.json({
-            error: err
+    // Determine ordinality
+    job.getTasks(function(err, tasks) {
+      if (err) return next(err);
+
+      async.reduce(tasks, -1, function(memo, item, cb) {
+        cb(null, Math.max(memo, item.ordinality));
+      }, function(err, maxOrdinality) {
+        if (err) return next(err);
+
+        var ordinality = maxOrdinality + 1;
+
+        job.addTasks(task, {
+          ordinality: ordinality
+        }, function(err) {
+          if (err) next(err);
+
+          res.json({
+            success: 'Added task to job'
           });
-
-        res.json({
-          success: 'Added task to job'
         });
       });
+    });
+  });
+});
+// TODO flatten
 
-      // TODO determine ordinality
+router.post('/:job/removetask/:task', function(req, res, next) {
+  db.models.Job.get(req.params.job, function(err, job) {
+    if (err) return next(err);
+
+    if (!job) {
+      //res.status(404);
+      res.json({
+        error: 'Job not found'
+      });
+      return;
+    }
+
+    var removeOrdinality = parseInt(req.params.task);
+
+    var toRemove = job.tasks.filter(function(task) {
+      return task.ordinality == removeOrdinality;
+    });
+
+    console.log(toRemove);
+    if (toRemove.length === 0) {
+      //res.status(404);
+      res.json({
+        error: 'Job-Task not found'
+      });
+      return;
+    }
+
+    job.removeTasks(toRemove, function(err) {
+      if (err) return  next(err);
+
+      res.json({
+        success: 'Task removed from job'
+      });
     });
   });
 });
