9ecb22695a79654692f431fa6355d8db35162d5f
jarmille <jarmille@umflint.edu>
2015-07-23 16:02:04 -0400

    Fixed recursion protection in JobResolver (again) and added HTTP endpoint for getting job data

--- engine/JobResolver.js
+++ engine/JobResolver.js
@@ -3,18 +3,23 @@ var async = require('async');
 var db = require('./Database');
 
 module.exports.resolve = function resolve(job, done, blacklist) {
+
   if (!job.model || job.model() !== db.models.Job) {
+    //if (typeof job !== 'number') return done(new Error('Invalid jobresolve request'));
+
     //  We need to get the job
     return db.models.Job.get(job, function(err, jobObj) {
       if (err) return done(err);
       if (!jobObj) return done(new Error('Resolver could not find a job for "' + job + '"'));
-      resolve(jobObj, done);
+      resolve(jobObj, done, blacklist);
     });
   }
 
+
+
   blacklist = blacklist || [];
-  if (blacklist.indexOf(job) !== -1) return done(new Error('Recursive job nesting'));
-  blacklist.push(job);
+  if (blacklist.indexOf(job.id) !== -1) return done(new Error('Recursive job nesting'));
+  blacklist.push(job.id);
 
   async.reduce(job.tasks, [], function(memo, task, cb) {
     if (task.action === 'job') {
--- routes/signs.js
+++ routes/signs.js
@@ -4,13 +4,16 @@ var router = module.exports = express.Router();
 
 var SignManager = require('../engine/SignManager');
 var db = require('../engine/database');
+var JobResolver = require('../engine/JobResolver');
 
 /* GET main signs page */
 router.get('/', function(req, res, next) {
 
   async.parallel([
     function(cb) {
-      db.models.Sign.find({state: 'ACTIVE'}, cb);
+      db.models.Sign.find({
+        state: 'ACTIVE'
+      }, cb);
     },
     function(cb) {
       db.models.Job.all(cb);
@@ -34,27 +37,61 @@ router.post('/:sign/setjob', function(req, res, next) {
   var jobID = parseInt(req.body.job);
 
   db.models.Sign.get(req.params.sign, function(err, sign) {
-    if (err) return res.json({error: err});
+    if (err) return res.json({
+      error: err
+    });
 
     if (jobID === -1)
       return sign.removeJob(function(err) {
-        if (err) return res.json({error: err});
-        res.json({success: 'Removed job from sign'});
+        if (err) return res.json({
+          error: err
+        });
+        res.json({
+          success: 'Removed job from sign'
+        });
       });
 
     db.models.Job.get(jobID, function(err, job) {
-      if (err) return res.json({error: err});
+      if (err) return res.json({
+        error: err
+      });
 
       sign.setJob(job, function(err) {
         if (err) {
-          res.json({error: err})
+          res.json({
+            error: err
+          })
         } else {
           res.json({
             success: 'Set new job for sign'
           });
         }
       });
-      
+
+    });
+  });
+});
+
+/* GET sign job flattened (from sign-client usually) */
+router.get('/:sign/getjobdata', function(req, res, next) {
+  db.models.Sign.get(req.params.sign, function(err, sign) {
+    if (err)
+      return res.json({
+        error: err
+      });
+
+    JobResolver.resolve(sign.job.id, function(err, job) {
+      if (err)
+      //throw err;
+        return res.json({
+        error: err.message
+      });
+
+      res.json({
+        success: true,
+        name: sign.name,
+        job: job
+      });
     });
   });
 });
